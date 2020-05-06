17849325ab92486e5bc17d7cf58e865c3b7e245a
Frank Hoedl <frankhoedl@mac.com>
2020-05-06 13:00:58 -0400

    Updates

---
+++ precache-manifest.7fe468da3ae1deb3fed233fdfc53b071.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a435b1ab3b3c089b63235cebcea39134",
+    "url": "/whoToldItBetter/index.html"
+  },
+  {
+    "revision": "fd3faa0aef16bd7862da",
+    "url": "/whoToldItBetter/static/css/main.0391848f.chunk.css"
+  },
+  {
+    "revision": "166d42173e31df9c0056",
+    "url": "/whoToldItBetter/static/js/2.b1180c65.chunk.js"
+  },
+  {
+    "revision": "413222bfbc1dccea2ee6791f9b56cfbf",
+    "url": "/whoToldItBetter/static/js/2.b1180c65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd3faa0aef16bd7862da",
+    "url": "/whoToldItBetter/static/js/main.9721599f.chunk.js"
+  },
+  {
+    "revision": "82a7a2c0dc173b87ee6e",
+    "url": "/whoToldItBetter/static/js/runtime-main.8150cbcc.js"
+  }
+]);
